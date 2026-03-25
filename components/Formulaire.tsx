2bf0536404e09aa4511e02175580b109a11ba1f0
MedAmine2221 <lazregamine258@gmail.com>
2026-03-25 21:05:24 +0100

    add modal for the quiz interview modal

--- components/Formulaire.tsx
+++ components/Formulaire.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { Button } from "@heroui/react";
+import { Button, Modal } from "@heroui/react";
 import PostDescription from "./PostDescription";
 import UploadCV from "./UploadCV";
 import { useState } from "react";
+import QuizModal from "./QuizModal";
 
 export default function Formulaire() {
     const [postDesc, setPostDesc] = useState("");
@@ -12,10 +13,13 @@ export default function Formulaire() {
     <>
         <PostDescription isPressed={isPressed} postDesc={postDesc} setPostDesc={setPostDesc} />
         <UploadCV isPressed={isPressed} cv = {cv} setCv={setCv} />
-        <Button onClick={()=> setIsPressed(true)
-        } className="bg-[#d99934]/10 border-2 border-[#d99934] rounded-lg m-4 w-fit h-12 text-[#d99934] font-bold">
-          Create a Virtual Interview
-        </Button>
+        <Modal>
+            <Button onClick={()=> setIsPressed(true)
+            } className="bg-[#d99934]/10 border-2 border-[#d99934] rounded-lg m-4 w-fit h-12 text-[#d99934] font-bold">
+              Create a Virtual Interview
+            </Button>
+            {cv && postDesc && isPressed && <QuizModal />}
+        </Modal>
     </>
 )
 }
---
+++ components/QuizModal.tsx
@@ -0,0 +1,33 @@
+import { Button, Modal } from '@heroui/react'
+import React from 'react'
+import { BiRocket } from 'react-icons/bi'
+
+export default function QuizModal() {
+  return (
+    
+      <Modal.Backdrop>
+        <Modal.Container>
+          <Modal.Dialog className="sm:max-w-[360px]">
+            <Modal.CloseTrigger />
+            <Modal.Header>
+              <Modal.Icon className="bg-default text-foreground">
+                <BiRocket className="size-5" />
+              </Modal.Icon>
+              <Modal.Heading>Welcome to HeroUI</Modal.Heading>
+            </Modal.Header>
+            <Modal.Body>
+              <p>
+                A beautiful, fast, and modern React UI library for building accessible and
+                customizable web applications with ease.
+              </p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button className="w-full" slot="close">
+                Continue
+              </Button>
+            </Modal.Footer>
+          </Modal.Dialog>
+        </Modal.Container>
+      </Modal.Backdrop>
+  )
+}
